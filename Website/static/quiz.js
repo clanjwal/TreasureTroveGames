6329fcb26e254b1563649d436936008e4815267d
salma-ghazii <148153677+salma-ghazii@users.noreply.github.com>
2023-11-05 01:58:23 -0400

    Delete Website/static/quiz.js

--- Website/static/quiz.js
+++
@@ -1,7 +0,0 @@
-function questions() {
-    var a1 = document.querySelector("input[name='q1']:checked").value;
-    if(a1 === "Blue Whale")
-        document.getElementById(results).innerHTML = "You got it correct!";
-    else
-        document.getElementById(results).innerHTML = "You got it wrong :(";
-}
